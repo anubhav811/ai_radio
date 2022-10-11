edc11b364d6774e8623d34e79986cbc98305372b
Anubhav Chachra <chachraanubhav@gmail.com>
2022-10-11 22:11:55 +0530

    voice

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Ai Radio",
-        theme: ThemeData( 
+        title: "Radio",
+        theme: ThemeData(
           fontFamily: GoogleFonts.poppins().fontFamily,
         ),
         home: HomePage());
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:ai_radio/utils/utils.dart';
 import 'package:alan_voice/alan_voice.dart';
 import 'package:audioplayers/audioplayers.dart';
@@ -15,19 +17,31 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State {
-  List<MyRadio> radios = [];
-  bool isPlaying = false;
+  List<MyRadio>? radios;
   MyRadio? _selectedRadio;
   Color? _selectedColor;
-  final AudioPlayer audioPlayer = AudioPlayer();
+  bool isPlaying = false;
+  final sugg = [
+    "Play",
+    "Stop",
+    "Play rock music",
+    "Play 107 FM",
+    "Play next",
+    "Play 104 FM",
+    "Pause",
+    "Play previous",
+    "Play pop music"
+  ];
+
+  final AudioPlayer _audioPlayer = AudioPlayer();
 
   @override
   void initState() {
     super.initState();
-    setUpAlan();
+    setupAlan();
     fetchRadios();
 
-    audioPlayer.onPlayerStateChanged.listen((event) {
+    _audioPlayer.onPlayerStateChanged.listen((event) {
       if (event == PlayerState.playing) {
         isPlaying = true;
       } else {
@@ -37,49 +51,115 @@ class HomePageState extends State {
     });
   }
 
-  setUpAlan() {
+  setupAlan() {
     AlanVoice.addButton(
         "487066de63d893e1246ea64f97594c1f2e956eca572e1d8b807a3e2338fdd0dc/stage",
-        buttonAlign: AlanVoice.BUTTON_ALIGN_LEFT);
-    AlanVoice.callbacks.add((command) => handleCommand(command.data));
+        buttonAlign: AlanVoice.BUTTON_ALIGN_RIGHT);
+    AlanVoice.callbacks.add((command) => _handleCommand(command.data));
   }
 
-  handleCommand(Map<String, dynamic> response) {
+  _handleCommand(Map<String, dynamic> response) {
     switch (response["command"]) {
       case "play":
-        _playMusic(_selectedRadio.url);
+        _playMusic(_selectedRadio!.url);
+        break;
+
+      case "play_channel":
+        final id = response["id"];
+        // _audioPlayer.pause();
+        MyRadio newRadio = radios!.firstWhere((element) => element.id == id);
+        radios!.remove(newRadio);
+        radios!.insert(0, newRadio);
+        _playMusic(newRadio.url);
+        break;
+
+      case "stop":
+        _audioPlayer.stop();
+        break;
+      case "next":
+        final index = _selectedRadio!.id;
+        MyRadio newRadio;
+        if (index + 1 > radios!.length) {
+          newRadio = radios!.firstWhere((element) => element.id == 1);
+          radios!.remove(newRadio);
+          radios!.insert(0, newRadio);
+        } else {
+          newRadio = radios!.firstWhere((element) => element.id == index + 1);
+          radios!.remove(newRadio);
+          radios!.insert(0, newRadio);
+        }
+        _playMusic(newRadio.url);
+        break;
+
+      case "prev":
+        final index = _selectedRadio!.id;
+        MyRadio newRadio;
+        if (index - 1 <= 0) {
+          newRadio = radios!.firstWhere((element) => element.id == 1);
+          radios!.remove(newRadio);
+          radios!.insert(0, newRadio);
+        } else {
+          newRadio = radios!.firstWhere((element) => element.id == index - 1);
+          radios!.remove(newRadio);
+          radios!.insert(0, newRadio);
+        }
+        _playMusic(newRadio.url);
         break;
       default:
+        print("Command was ${response["command"]}");
         break;
     }
   }
 
-  void fetchRadios() async {
+  fetchRadios() async {
     final radioJson = await rootBundle.loadString("assets/files/radio.json");
     final decodedData = jsonDecode(radioJson);
     var radiosData = decodedData["radios"];
     radios = List.from(radiosData)
         .map<MyRadio>((item) => MyRadio.fromMap(item))
         .toList();
-    _selectedRadio = radios[0];
-    _selectedColor = Color(int.tryParse(_selectedRadio.color));
+    _selectedRadio = radios![0];
+    _selectedColor = Color(int.parse(_selectedRadio!.color));
     setState(() {});
   }
 
   _playMusic(String url) {
-    UrlSource urlSource = UrlSource(url);
-    audioPlayer.play(urlSource);
-    _selectedRadio = radios.firstWhere((element) => element.url == url);
+    _audioPlayer.play(UrlSource(url));
+    _selectedRadio = radios!.firstWhere((element) => element.url == url);
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(),
+      drawer: Drawer(
+        child: Container(
+          color: _selectedColor ?? AIColors.primaryColor2,
+          child: radios != null
+              ? [
+                  100.heightBox,
+                  "All Channels".text.xl.white.semiBold.make().px16(),
+                  20.heightBox,
+                  ListView(
+                    padding: Vx.m0,
+                    shrinkWrap: true,
+                    children: radios!
+                        .map((e) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(e.icon),
+                              ),
+                              title: "${e.name} FM".text.white.make(),
+                              subtitle: e.tagline.text.white.make(),
+                            ))
+                        .toList(),
+                  ).expand()
+                ].vStack(crossAlignment: CrossAxisAlignment.start)
+              : const Offstage(),
+        ),
+      ),
       body: Stack(
         fit: StackFit.expand,
-        // ignore: sort_child_properties_last
+        clipBehavior: Clip.antiAlias,
         children: [
           VxAnimatedBox()
               .size(context.screenWidth, context.screenHeight)
@@ -94,44 +174,62 @@ class HomePageState extends State {
                 ),
               )
               .make(),
-          AppBar(
-            title: "Radio".text.xl4.bold.white.make().shimmer(
-                primaryColor: Vx.purple300, secondaryColor: Colors.white),
-            backgroundColor: Colors.transparent,
-            centerTitle: true,
-            elevation: 0,
-          ).h(100).p16(),
-          // ignore: unnecessary_null_comparison
+          [
+            AppBar(
+              title: "Radio".text.xl4.bold.white.make().shimmer(
+                  primaryColor: Vx.purple300, secondaryColor: Colors.white),
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              centerTitle: true,
+            ).h(100.0).p16(),
+            "Start with - Hey Alan 👇".text.italic.semiBold.white.make(),
+            10.heightBox,
+            VxSwiper.builder(
+              itemCount: sugg.length,
+              height: 50.0,
+              viewportFraction: 0.35,
+              autoPlay: true,
+              autoPlayAnimationDuration: 3.seconds,
+              autoPlayCurve: Curves.linear,
+              enableInfiniteScroll: true,
+              itemBuilder: (context, index) {
+                final s = sugg[index];
+                return Chip(
+                  label: s.text.make(),
+                  backgroundColor: Vx.randomColor,
+                );
+              },
+            )
+          ].vStack(alignment: MainAxisAlignment.start),
+          30.heightBox,
           radios != null
               ? VxSwiper.builder(
-                  itemCount: radios.length,
-                  aspectRatio: 1,
+                  itemCount: radios!.length,
+                  aspectRatio: 1.5,
+                  enlargeCenterPage: true,
                   onPageChanged: (index) {
-                    final colorHex = radios[index].color;
+                    _selectedRadio = radios![index];
+                    final colorHex = radios![index].color;
                     _selectedColor = Color(int.parse(colorHex));
                     setState(() {});
                   },
-                  enlargeCenterPage: true,
                   itemBuilder: (context, index) {
-                    final rad = radios[index];
+                    final rad = radios![index];
+
                     return VxBox(
                             child: ZStack(
                       [
                         Positioned(
-                          top: 5.0,
-                          right: 5.0,
+                          top: 0.0,
+                          right: 0.0,
                           child: VxBox(
-                            child: rad.category.text
-                                .size(6)
-                                .uppercase
-                                .white
-                                .make(),
+                            child:
+                                rad.category.text.uppercase.white.make().px16(),
                           )
-                              .height(65)
-                              .width(65)
+                              .height(40)
                               .black
                               .alignCenter
-                              .withRounded(value: 360)
+                              .withRounded(value: 10.0)
                               .make(),
                         ),
                         Align(
@@ -166,12 +264,12 @@ class HomePageState extends State {
                                   Colors.black.withOpacity(0.3),
                                   BlendMode.darken)),
                         )
-                        .border(color: Colors.black, width: 2.0)
-                        .withRounded(value: 0.0)
+                        .border(color: Colors.black, width: 5.0)
+                        .withRounded(value: 60.0)
                         .make()
                         .onInkDoubleTap(() {
                       _playMusic(rad.url);
-                    }).px1();
+                    }).p16();
                   },
                 ).centered()
               : const Center(
@@ -183,7 +281,7 @@ class HomePageState extends State {
             alignment: Alignment.bottomCenter,
             child: [
               if (isPlaying)
-                "Playing Now - ${_selectedRadio.name} FM"
+                "Playing Now - ${_selectedRadio!.name} FM"
                     .text
                     .white
                     .makeCentered(),
@@ -195,15 +293,14 @@ class HomePageState extends State {
                 size: 50.0,
               ).onInkTap(() {
                 if (isPlaying) {
-                  audioPlayer.stop();
+                  _audioPlayer.stop();
                 } else {
-                  _playMusic(_selectedRadio.url);
+                  _playMusic(_selectedRadio!.url);
                 }
               })
             ].vStack(),
           ).pOnly(bottom: context.percentHeight * 12)
         ],
-        clipBehavior: Clip.antiAlias,
       ),
     );
   }
